6c790deb1c9ab826bb1fb8e2b6745785f08566ab
Kristian Emil <79602784+svidt@users.noreply.github.com>
2025-01-24 13:19:17 +0100

    Create challenge-01.swift

---
+++ challenge-01.swift
@@ -0,0 +1,8 @@
+// Celcius to Fahrenheit converter
+import UIKit
+
+let temperatureInCelsius: Double = 25.0
+let temperatureInFahrenheit: Double = temperatureInCelsius * 9.0 / 5.0 + 32.0
+
+print("temperatureInCelsius: \(temperatureInCelsius)")
+print("temperatureInFahrenheit: \(temperatureInFahrenheit)")
